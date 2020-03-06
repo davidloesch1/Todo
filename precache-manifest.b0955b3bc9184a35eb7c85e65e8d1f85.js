89000b20c7d2ff8abce561d079d5c89cd80c1668
David Loesch <davidloesch1@gmail.com>
2020-03-06 13:59:00 -0800

    Updates

---
+++ precache-manifest.b0955b3bc9184a35eb7c85e65e8d1f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c95304839b5d1d0a66d2c0a67db08e",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "6d07f3b05bd0fb207738",
+    "url": "/todo/static/css/main.d7fff8bb.chunk.css"
+  },
+  {
+    "revision": "ab11cd20c4054a725666",
+    "url": "/todo/static/js/2.9f3b22ca.chunk.js"
+  },
+  {
+    "revision": "df84d6d8f1e6d347c6c3d50dd7c56841",
+    "url": "/todo/static/js/2.9f3b22ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d07f3b05bd0fb207738",
+    "url": "/todo/static/js/main.70575e69.chunk.js"
+  },
+  {
+    "revision": "a65703e962574574e50d",
+    "url": "/todo/static/js/runtime-main.3669824a.js"
+  }
+]);
